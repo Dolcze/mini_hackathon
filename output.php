cabb0f0ad0bc3301b0f8f8821d0abb0fc2b54633
Dolcze_PC_LOCAL <adamo.jask@gmail.com>
2021-12-13 14:05:52 +0100

    minor changes when outputing

--- output.php
+++ output.php
@@ -4,7 +4,20 @@ include_once("header.php");
 if (isset($_GET["info"]) && isset($_GET["bin_type"])) {
     // pokaz informacje
     echo $_GET['info'];
-    echo $_GET['bin_type'];
+
+    if ($_GET["bin_type"] == 0){
+        echo "mieszane";
+    }
+    if ($_GET["bin_type"] == 1){
+        echo "plastik i metal";
+    }
+    if ($_GET["bin_type"] == 2){
+        echo "papier";
+    }
+    if ($_GET["bin_type"] == 3){
+        echo "szkło";
+    }
+
     echo $_GET['code'];
 }else{
     // wyszedl jakis error
